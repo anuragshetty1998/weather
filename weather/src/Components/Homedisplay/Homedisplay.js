e2fbcd49e086da4515ff4b62a3d8a88dae3323dc
anuragshetty1998 <shettyanurag9@gmail.com>
2021-08-17 03:06:44 +0530

    home fav local complete

--- weather/src/Components/Homedisplay/Homedisplay.js
+++ weather/src/Components/Homedisplay/Homedisplay.js
@@ -1,21 +1,68 @@
 import React, { useState, useEffect } from "react";
 import "./Homedisplay.css";
 import Homedetails from "../Homedetails/Homedetails.js";
-import { AiOutlineHeart } from "react-icons/ai";
+//import { AiOutlineHeart } from "react-icons/ai";
 import sunny from "../../assets/sunny.png";
+import favheart from "../../assets/favheart.png";
+import notfavheart from "../../assets/notfavheart.png";
 
 const Homedisplay = ({ urlData, setTempUnit }) => {
   const [unit, setUnit] = useState("metric");
+  const [favIcon, setFavIcon] = useState("notfav");
+  const [favList, setFavList] = useState(() => {
+    let list = localStorage.getItem("localFav");
+    if (list) {
+      return JSON.parse(localStorage.getItem("localFav"));
+    } else {
+      return [];
+    }
+  });
+
+  let city = urlData.name;
   useEffect(() => {
     setTempUnit(unit);
   }, [unit]);
+
+  console.log("city", city);
+
+  useEffect(() => {
+    if (favList.includes(city)) {
+      setFavIcon("fav");
+      console.log("fav-select");
+    } else {
+      setFavIcon("notfav");
+    }
+  }, [favIcon, urlData]);
+
+  console.log(favList);
+
+  const handleFavClick = () => {
+    if (favIcon === "fav") {
+      setFavIcon("notfav");
+      setFavList(favList.filter((element) => element !== city));
+    } else {
+      setFavList([...favList, city]);
+      setFavIcon("fav");
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("localFav", JSON.stringify(favList));
+  }, [city, favIcon]);
 
   return (
     <div className="homedis-div">
       <div className="home-place">
         <p>{`${urlData.name}, ${urlData.sys.country}`}</p>
         <div className="addfav-div">
-          <AiOutlineHeart fontSize="1.9rem" color="white" />
+          {
+            <img
+              src={favIcon === "fav" ? favheart : notfavheart}
+              alt="fav"
+              onClick={handleFavClick}
+            />
+          }
+
           <p>Add to favourite</p>
         </div>
       </div>
--- weather/src/Route/Route.js
+++ weather/src/Route/Route.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-// import Favourite from "../components/favourite/Favourite";
+//import Favourite from "../components/favourite/Favourite";
 // import RecentSearch from "../components/recentSearch/RecentSearch";
 import Home from "../Container/Home/Home.js";
 
@@ -11,8 +11,8 @@ const Routing = ({ searchTerm, searchStart }) => {
         <Route exact path="/">
           <Home searchTerm={searchTerm} searchStart={searchStart} />
         </Route>
-        {/* <Route path="/favourite" component={Favourite} />
-        <Route path="/recent-search" component={RecentSearch} /> */}
+        {/* <Route path="/favourite" component={Favourite} /> */}
+        {/*  <Route path="/recent-search" component={RecentSearch} /> */}
       </Switch>
     </>
   );
